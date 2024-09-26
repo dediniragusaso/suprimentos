1b236f3581bc938aa3d609964ba4aae8c1e661f9
Sophia Dedini Ragusa <sophia.ragusa@germinare.org.br>
2024-09-26 14:36:25 -0300

    Subindo a nova estrura para teste

---
+++ teste4.py
@@ -0,0 +1,192 @@
+import openai
+import time
+import tiktoken
+from dotenv import load_dotenv
+import os
+from flask import Flask, render_template,  request, jsonify, Response, stream_with_context
+from logging import ERROR
+# #Funções
+from logging import basicConfig #configurações para os comportamentos dos logs
+from logging import error
+from logging import getLogger
+import tiktoken
+basicConfig(
+    level = ERROR  , #Todas as informações com maior ou prioridade igual ao DEBUG serão armazenadas
+    filename= "logs.log", #Onde serão armazenadas as informações
+    filemode= "a", # Permissões do arquivo [se poderá editar, apenas ler ...]
+    format= '%(levelname)s->%(asctime)s->%(message)s->%(name)s' # Formatação da informação
+)
+getLogger('openai').setLevel(ERROR)
+getLogger('werkzeug').setLevel(ERROR)
+
+load_dotenv()
+api_key = os.getenv("openai_api_key")
+if not api_key:
+    raise ValueError("Chave API não encontrada. Verifique se 'openai_api_key' está definida no ambiente.")
+
+openai.api_key = api_key
+
+# variáveis globais
+categorizador_prompt= open('./prompts/indicador_prompt.txt', "r", encoding="utf8").read()
+escritor= open('./prompts/escritor_prompt.txt',"r",encoding="utf8").read() 
+
+encoding = tiktoken.encoding_for_model("gpt-4o-2024-08-06")
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/limparTerminal", methods=["POST"])
+def limparTerminal():
+    global cont_requisicao
+    reiniciar = request.json["recarregadoPorBotao"]
+    if reiniciar == True:
+        cont_requisicao = 0
+        return jsonify({"status": "success"})
+    else:
+        return jsonify({"status": "success"})
+    
+
+
+# função quando dá erro
+def algo_ocorreu_de_errado():
+    yield "Algo ocorreu de errado, tente novamente"
+
+def categorizador(prompt_usuario):
+    prompt=categorizador_prompt
+    print (type(prompt))
+    for arq in (os.listdir("./bases")):
+        prompt += open(f"./bases/{arq}","r",encoding="utf8").read()  
+    tentativas = 0
+    tempo_de_espera = 5
+    while tentativas <3:
+        tentativas+=1
+        try:
+            resposta = openai.ChatCompletion.create(
+                model = "gpt-4o-2024-08-06",
+                messages = [
+                    {
+                        "role": "system",
+                        "content": prompt
+                    },
+                    {
+                        "role": "user",
+                        "content": prompt_usuario
+                    }
+                ],
+                temperature=0.7,
+                max_tokens=100,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+            )
+
+            
+            print("Análise realizada com sucesso")
+            return resposta.choices[0].message.content
+        except openai.error.AuthenticationError as e:
+            print(f"Erro de autentificação {e}")
+        except openai.error.APIError as e:
+            print(f"Erro de API {e}")
+            time.sleep(5)
+        except openai.error.RateLimitError as e:
+            print(f"Erro de limite de taxa: {e}")
+            time.sleep(tempo_de_espera)
+            tempo_de_espera *=2
+
+
+def resposta (prompt_usuario, historico, nome_arquivo):
+    prompt=escritor
+    prompt+= open(f"./bases/{nome_arquivo}","r",encoding="utf8").read()
+    prompt += historico
+    tentativas = 0
+    tempo_de_espera = 5
+    while tentativas <3:
+        tentativas+=1
+        print(f"Tentativa {tentativas}")
+        print(prompt)
+        try:
+            print(f"Iniciando a análise")
+            resposta = openai.ChatCompletion.create(
+                model = "gpt-4o-2024-08-06",
+                messages = [
+                    {
+                        "role": "system",
+                        "content": prompt
+                    },
+                    {
+                        "role": "user",
+                        "content": prompt_usuario
+                    }
+                ],
+                temperature=0.,
+                max_tokens=5000,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                stream=True
+            )
+            
+            print("Resposta feita com sucesso")
+            for chunk in resposta:
+                    if 'choices' in chunk and 'delta' in chunk['choices'][0] and 'content' in chunk['choices'][0]['delta']:
+                        text = chunk['choices'][0]['delta']['content']
+                        if text:
+                            print(text, end="")
+                            yield text
+
+            return
+        except openai.error.AuthenticationError as e:
+            print(f"Erro de autentificação {e}")
+        except openai.error.APIError as e:
+            print(f"Erro de API {e}")
+            time.sleep(5)
+        except openai.error.RateLimitError as e:
+            print(f"Erro de limite de taxa: {e}")
+            time.sleep(tempo_de_espera)
+            tempo_de_espera *=2
+
+
+arquivos=[]
+for arq in (os.listdir("./bases")):
+        arquivos.append(arq) 
+@app.route("/submit", methods=["POST"])
+
+def submit():
+    
+    try:
+        historico = request.form['historico']
+        pergunta_usuario = request.form['inputMessage']
+        base = categorizador(pergunta_usuario)
+        if ( base in arquivos ):
+            # model ='gpt-4o-2024-08-06'
+            # enc = tiktoken.encoding_for_model(model)
+            # input= pergunta_usuario
+            # arquivo=open(f'./bases/{base}', "r", encoding="utf8").read()
+            # output= resposta+arquivo
+            # for arq in (os.listdir("./bases")):
+            #     output += open(f"./bases/{arq}","r",encoding="utf8").read() 
+            # resposta= Response(stream_with_context(resposta(pergunta_usuario,historico,base)), content_type='text/plain')
+
+            # #contagem
+            # tokens_input = len(enc.encode(input))
+            # tokens_output = len(enc.encode(output))
+            # custo_input = 0.0025
+ 
+            # custo_output = 0.01
+ 
+            # custo_total = tokens_input/1000*custo_input + tokens_output/1000*custo_output
+ 
+            # print(custo_total)
+            return Response(stream_with_context(resposta(pergunta_usuario,historico,base)), content_type='text/plain')
+        
+        else: 
+             print("Erro")
+    except Exception as e:
+        error(e)
+        return Response(stream_with_context(algo_ocorreu_de_errado()), content_type='text/plain')
+    
+app.run(debug=True, port=5000)
+
