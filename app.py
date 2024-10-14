cfe6d9b4e031e0db0ccad59be778e62c0d714422
guibs <guilherme.barbosa@germinare.org.br>
2024-10-14 10:20:40 -0300

    adicionando banco de dados e arrumando pdfs

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Importações
 import openai
 import time
 import tiktoken
@@ -5,13 +6,15 @@ from dotenv import load_dotenv
 import os
 import re
 from flask import Flask, render_template,  request, jsonify, Response, stream_with_context
+import tiktoken
+import requests 
+import psycopg2
+
+# LOG
 from logging import ERROR
-# #Funções
 from logging import basicConfig #configurações para os comportamentos dos logs
 from logging import error
 from logging import getLogger
-import tiktoken
-import requests 
 
 basicConfig(
     level = ERROR  , #Todas as informações com maior ou prioridade igual ao DEBUG serão armazenadas
@@ -22,6 +25,7 @@ basicConfig(
 getLogger('openai').setLevel(ERROR)
 getLogger('werkzeug').setLevel(ERROR)
 
+# Variáveis de ambiente
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 correct_password = os.getenv("CORRECT_PASSWORD")
@@ -30,39 +34,67 @@ if not api_key:
 
 openai.api_key = api_key
 
-def inserir_banco(query):
+# Método para conectar no banco
+def conexao_banco():
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PSWD"),
+            port=os.getenv("DB_PORT")
+        )
+        print("Conexão feita com sucesso!")
+        return conn
+    except psycopg2.Error as e:
+        print(f"Erro ao conectar no banco de dados: {e}")
+        return None
 
-    conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT")
-    )
 
-    cursor = conn.cursor()
-    cursor.execute(query)
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-# variáveis globais
+# Variáveis Globais
 categorizador_prompt= open('./prompts/indicador_prompt.txt', "r", encoding="utf8").read()
 escritor= open('./prompts/escritor_prompt.txt',"r",encoding="utf8").read() 
 erro = open('./prompts/erro.txt',"r",encoding="utf8").read() 
 normas= open('./prompts/prompt_normas.txt', "r", encoding="utf8").read()
+chat_id = 0
 
 encoding = tiktoken.encoding_for_model("gpt-4o-2024-08-06")
 
 app = Flask(__name__)
 
+# Página Raiz
 @app.route('/')
 def index():
+    global chat_id
+    
+    try:
+        conn = conexao_banco()
+        cursor = conn.cursor()
+        
+        # inserindo um novo chat
+        cursor.execute("""INSERT INTO CHATS(DT_CHAT, NR_PERGUNTAS, NR_RESPOSTAS) 
+                        VALUES (NOW(), 0, 0)
+                        RETURNING ID_CHAT;""")
+        chat_id = cursor.fetchone()[0]
+        
+        cursor.execute("""INSERT INTO CONTROLE(CD_CHAT, NR_TOKENS_PERG, NR_TOKENS_RESP, VL_DOLAR)
+                        VALUES(%s, 0, 0, 0)""",(chat_id,))
+        
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        print(f"Erro: {e}")
+    finally:
+        cursor.close()
+        conn.close()
+        
     return render_template('index.html')
 
+# Forms de Login
 @app.route('/login', methods=["POST"])
 def login():
     try:
+        # verificação de senha
         password = request.json["password"]
         if password == correct_password:
             return jsonify({"status": "success"}),200
@@ -71,6 +103,7 @@ def login():
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}),500
 
+# Recarregar página
 @app.route("/limparTerminal", methods=["POST"])
 def limparTerminal():
     global cont_requisicao
@@ -82,20 +115,22 @@ def limparTerminal():
         return jsonify({"status": "success"})
     
 
-
-# função quando dá erro
+# Mensagem de erro para o usuário
 def algo_ocorreu_de_errado():
     yield "Algo ocorreu de errado, tente novamente"
 
+# Contar o número de tokens
 def contar_tokens(texto):
     return len(encoding.encode(texto))
 
+# Categorizar mensagem em apenas um procedimento
 def categorizador(prompt_usuario, api_key):
+    global chat_id
     prompt_100 = open('./prompts/indicador_prompt.txt', "r", encoding="utf8").read()
 
 
-    for arq in os.listdir("./prompts/palavras_chaves/bases_100"):
-        prompt_100 += f"\n\n{arq}\n" + open(f"./prompts/palavras_chaves/bases_100/{arq}", "r", encoding="utf8").read()
+    for arq in os.listdir("./prompts/palavras_chaves"):
+        prompt_100 += f"\n\n{arq}\n" + open(f"./prompts/palavras_chaves/{arq}", "r", encoding="utf8").read()
 
 
     headers = {
@@ -122,25 +157,53 @@ def categorizador(prompt_usuario, api_key):
 
     payload["messages"][0]["content"] = prompt_100
     resposta = requests.post("https://api.openai.com/v1/chat/completions",headers=headers, json=payload)
-    print(resposta.json())
-    return resposta.json()["choices"][0]["message"]["content"]
+    
+    nr_tokens_perg, nr_tokens_resp = resposta.json()["usage"]["prompt_tokens"], resposta.json()["usage"]["completion_tokens"]
+    chat_custo = (nr_tokens_perg/1000*0.0025) + (nr_tokens_resp/1000*0.01)
+    resposta = resposta.json()["choices"][0]["message"]["content"]  
+    print(chat_custo)   
+    print(resposta)
+    
+    try:
+        conn = conexao_banco()
+        cursor = conn.cursor()
+    
+        # Associar o procedimento ao chat
+        cursor.execute("""SELECT ID_PROCEDIMENTO FROM PROCEDIMENTOS
+                            WHERE NM_PROCEDIMENTO = %s;""", (resposta.replace(".txt",""),)) 
+        procedimento_id =  cursor.fetchone()[0]
+        cursor.execute("""INSERT INTO PROCEDIMENTOS_CHAT(CD_CHAT, CD_PROCEDIMENTO)
+                            VALUES (%s, %s);""", (chat_id, procedimento_id))
+        
+        # Atualizar o custo do chat
+        cursor.execute("""UPDATE CONTROLE SET 
+                          NR_TOKENS_PERG = NR_TOKENS_PERG + %s, 
+                          NR_TOKENS_RESP = NR_TOKENS_RESP + %s,
+                          VL_DOLAR = VL_DOLAR + %s
+                          WHERE CD_CHAT = %s;""", (nr_tokens_perg, nr_tokens_resp, chat_custo, chat_id))
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        print(f"Erro: {e}")
+    finally:
+        cursor.close()
+        conn.close()
 
-       
+    return resposta
 
-
+# Responder a pergunta baseado no procedimento       
 def resposta (prompt_usuario, historico, nome_arquivo):
+    global chat_id
     prompt=escritor
     prompt+= open(f"./bases/{nome_arquivo}","r",encoding="utf8").read()
     prompt += historico
     
     tentativas = 0
     tempo_de_espera = 5
-    tokens_input= contar_tokens(prompt)
-    print(f"Entrada:{tokens_input}")
+    nr_tokens_perg= contar_tokens(prompt)
     while tentativas <3:
         tentativas+=1
         print(f"Tentativa {tentativas}")
-        print(prompt)
         try:
             print(f"Iniciando a análise")
             resposta = openai.ChatCompletion.create(
@@ -172,8 +235,27 @@ def resposta (prompt_usuario, historico, nome_arquivo):
                             output+=text
                             yield text
 
-            tokens_output = contar_tokens(output)
-            print(f"Tokens de saída: {tokens_output}")               
+            nr_tokens_resp = contar_tokens(output)
+            chat_custo = (nr_tokens_perg/1000*0.0025) + (nr_tokens_resp/1000*0.01)
+            print(chat_custo)
+            
+            try:
+                conn = conexao_banco()
+                cursor = conn.cursor()
+            
+                #Atualizar custo do chat
+                cursor.execute("""UPDATE CONTROLE SET 
+                                NR_TOKENS_PERG = NR_TOKENS_PERG + %s, 
+                                NR_TOKENS_RESP = NR_TOKENS_RESP + %s,
+                                VL_DOLAR = VL_DOLAR + %s
+                                WHERE CD_CHAT = %s;""", (nr_tokens_perg, nr_tokens_resp, chat_custo, chat_id))
+                conn.commit()
+            except Exception as e:
+                conn.rollback()
+                print(f"Erro: {e}")
+            finally:
+                cursor.close()
+                conn.close()            
 
             return
         except openai.error.AuthenticationError as e:
@@ -187,12 +269,11 @@ def resposta (prompt_usuario, historico, nome_arquivo):
             tempo_de_espera *=2
 
 
-
+# Pedir mais informações ao usuário
 def respostaErro (prompt_usuario, historico):
     prompt=erro
     prompt += historico
-    tokens_input= contar_tokens(prompt)
-    print(f"Entrada:{tokens_input}")
+    nr_tokens_perg= contar_tokens(prompt)
     tentativas = 0
     tempo_de_espera = 5
     while tentativas <3:
@@ -228,9 +309,29 @@ def respostaErro (prompt_usuario, historico):
                         if text:
                             output+=text
                             yield text
+            
+            nr_tokens_resp = contar_tokens(output)
+            chat_custo = (nr_tokens_perg/1000*0.0025) + (nr_tokens_resp/1000*0.01)
+            print(chat_custo)
+            
+            try:
+                conn = conexao_banco()
+                cursor = conn.cursor()
 
-            tokens_output = contar_tokens(output)
-            print(f"Tokens de saída: {tokens_output}") 
+                # Atualizar custo do chat
+                cursor.execute("""UPDATE CONTROLE SET 
+                                NR_TOKENS_PERG = NR_TOKENS_PERG + %s, 
+                                NR_TOKENS_RESP = NR_TOKENS_RESP + %s,
+                                VL_DOLAR = VL_DOLAR + %s
+                                WHERE CD_CHAT = %s;""", (nr_tokens_perg, nr_tokens_resp, chat_custo, chat_id))
+                conn.commit()
+            except Exception as e:
+                conn.rollback()
+                print(f"Erro: {e}")
+            finally:
+                cursor.close()
+                conn.close()    
+            
 
             return
         except openai.error.AuthenticationError as e:
@@ -243,13 +344,13 @@ def respostaErro (prompt_usuario, historico):
             time.sleep(tempo_de_espera)
             tempo_de_espera *=2
 
+# Substituir normas pelo conteúdo correspondente
 def substituidorNormas (resp,historico,pergunta_usuario,norma):
     prompt=normas
     prompt += historico
     prompt += ''.join(resp)  # junta todas as strings geradas por 'resp'
     prompt += open(f"./bases_normas/{norma}.txt","r",encoding="utf8").read() 
-    tokens_input= contar_tokens(prompt)
-    print(f"Entrada:{tokens_input}")
+    nr_tokens_perg= contar_tokens(prompt)
     tentativas = 0
     tempo_de_espera = 5
     while tentativas <3:
@@ -285,10 +386,29 @@ def substituidorNormas (resp,historico,pergunta_usuario,norma):
                         if text:
                             output+=text
                             yield text
-
-            tokens_output = contar_tokens(output)
-            print(f"Tokens de saída: {tokens_output}") 
-
+            
+            nr_tokens_resp = contar_tokens(output)
+            chat_custo = (nr_tokens_perg/1000*0.0025) + (nr_tokens_resp/1000*0.01)
+            print(chat_custo)
+            
+            try:
+                conn = conexao_banco()
+                cursor = conn.cursor()
+            
+                # Atualizar custo do chat
+                cursor.execute("""UPDATE CONTROLE SET 
+                                NR_TOKENS_PERG = NR_TOKENS_PERG + %s, 
+                                NR_TOKENS_RESP = NR_TOKENS_RESP + %s,
+                                VL_DOLAR = VL_DOLAR + %s
+                                WHERE CD_CHAT = %s;""", (nr_tokens_perg, nr_tokens_resp, chat_custo, chat_id))
+                conn.commit()
+            except Exception as e:
+                conn.rollback()
+                print(f"Erro: {e}")
+            finally:
+                cursor.close()
+                conn.close()    
+        
             return
         except openai.error.AuthenticationError as e:
             print(f"Erro de autentificação {e}")
@@ -305,42 +425,62 @@ for arq in (os.listdir("./bases")):
         arquivos.append(arq) 
 
 
-
+# Resposta do usuário
 @app.route("/submit", methods=["POST"])
  
 def submit():
     
     try:
-    
+        
+        global chat_id
+        
+        # Obter historico e pergunta do usuário
         historico = request.form['historico']
         pergunta_usuario = request.form['inputMessage']
     
+        # Categorizar a pergunta
         base = categorizador(pergunta_usuario, api_key)
+        
+        # Gerar a resposta
         resposta_sem_normas = resposta(pergunta_usuario, historico, base)
         if ( base and base in arquivos ):
             print("Base encontrada")
+            
+            # Verificar se há norma
             string_sem_espacos = ''.join(parte.replace(" ", "").replace("\n", "") for parte in resposta_sem_normas)
-
-            norma = re.search(r'(IN|M)-.*[0-9]{4}', string_sem_espacos, re.IGNORECASE)
+            norma = re.search(r'^(IN|M)-.*[0-9]{4}$', string_sem_espacos, re.IGNORECASE)
             if norma:
                 print("Baseado em norma")
                 regra = norma.group()  
-                print(regra)
                 
-                return Response(stream_with_context(substituidorNormas(resposta_sem_normas, historico, pergunta_usuario,regra,)), content_type='text/plain')
+                return Response(stream_with_context(substituidorNormas(resposta_sem_normas, historico, pergunta_usuario,regra)), content_type='text/plain')
             
             else:
-                print("nope")
-                print(type(resposta_sem_normas))
             
                 return Response(stream_with_context(resposta(pergunta_usuario, historico, base)), content_type='text/plain')
             
-           
-     
         else: 
             return Response(stream_with_context(respostaErro(pergunta_usuario,historico)), content_type='text/plain')
     except Exception as e:
         error(e)
         return Response(stream_with_context(algo_ocorreu_de_errado()), content_type='text/plain')
-    
+    finally:
+        
+        try:
+            conn = conexao_banco()
+            cursor = conn.cursor()
+            # Atualizar fluxo do chat
+            cursor.execute("""UPDATE CHATS SET 
+                            NR_PERGUNTAS = NR_PERGUNTAS + %s,  
+                            NR_RESPOSTAS = NR_RESPOSTAS + %s 
+                            WHERE ID_CHAT = %s;""", (1,1,chat_id))
+            conn.commit()
+        except Exception as e:
+            conn.rollback()
+            print(f"Erro: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+
+# Executar o servidor
 app.run(debug=True, port=5000, host="0.0.0.0")
