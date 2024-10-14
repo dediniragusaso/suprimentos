cbbcd67c042c04424f0693c20544ccce1bc73f70
guibs <guilherme.barbosa@germinare.org.br>
2024-10-14 10:34:14 -0300

    ajeitando conexão

--- app.py
+++ app.py
@@ -37,13 +37,7 @@ openai.api_key = api_key
 # Método para conectar no banco
 def conexao_banco():
     try:
-        conn = psycopg2.connect(
-            host=os.getenv("DB_HOST"),
-            database=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PSWD"),
-            port=os.getenv("DB_PORT")
-        )
+        conn = psycopg2.connect(os.getenv("DB_LINK"))
         print("Conexão feita com sucesso!")
         return conn
     except psycopg2.Error as e:
