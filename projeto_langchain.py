5bdadb97ac5a6d561840719abbd8dad0cf09bdf4
sophiekumagai <sophie.satie20@gmail.com>
2024-10-18 15:42:03 -0300

    teste

--- projeto_langchain.py
+++ projeto_langchain.py
@@ -81,7 +81,7 @@ erro = open('./prompts/erro.txt',"r",encoding="utf8").read()
 normas= open('./prompts/prompt_normas.txt', "r", encoding="utf8").read()
 
 # Inicializar a memória de conversação
-memory = ConversationBufferMemory(memory_key="history", return_messages=True)
+memory = ConversationBufferMemory(memory_key="history")
 
 encoding = tiktoken.encoding_for_model("gpt-4o-2024-08-06")
 
@@ -158,7 +158,7 @@ def resposta (prompt_usuario, historico, nome_arquivo):
             ])
 
             # Gera a resposta do modelo
-            resposta = llm.predict(prompt_template.format_prompt(history=memory.load_memory_variables({}), prompt_usuario=prompt_usuario))
+            resposta = llm.invoke(prompt_template.format_prompt(history=memory.buffer_as_messages, prompt_usuario=prompt_usuario))
 
             # Atualiza o histórico com a resposta gerada
             memory.save_context(inputs=[HumanMessage(content=prompt_usuario), AIMessage(content="")], outputs=[HumanMessage(content=prompt_usuario), AIMessage(content=resposta)])
@@ -213,8 +213,11 @@ def respostaErro (prompt_usuario, historico):
                 HumanMessagePromptTemplate.from_template("{prompt_usuario}")
             ])
 
-            # Gerar a resposta do modelo de linguagem
-            resposta = llm.invoke(prompt_template.format_prompt(history=memory.load_memory_variables({})["history"], prompt_usuario=prompt_usuario))
+            # Gera a resposta do modelo
+            resposta = llm.invoke(prompt_template.format_prompt(history=memory.buffer_as_messages, prompt_usuario=prompt_usuario))
+
+            # Atualiza o histórico com a resposta gerada
+            memory.save_context(inputs=[HumanMessage(content=prompt_usuario), AIMessage(content="")], outputs=[HumanMessage(content=prompt_usuario), AIMessage(content=resposta)])
             
             print("Resposta feita com sucesso")
             print(resposta)
@@ -262,8 +265,11 @@ def substituidorNormas (resp,historico,pergunta_usuario,norma):
                 HumanMessagePromptTemplate.from_template("{pergunta_usuario}")
             ])
 
-            # Gerar a resposta do modelo de linguagem
-            resposta = llm.predict(prompt_template.format_prompt(history=memory.load_memory_variables({})["history"], pergunta_usuario=pergunta_usuario))
+            # Gera a resposta do modelo
+            resposta = llm.invoke(prompt_template.format_prompt(history=memory.buffer_as_messages, pergunta_usuario=pergunta_usuario))
+
+            # Atualiza o histórico com a resposta gerada
+            memory.save_context(inputs=[HumanMessage(content=pergunta_usuario), AIMessage(content="")], outputs=[HumanMessage(content=pergunta_usuario), AIMessage(content=resposta)])
             
             print("Resposta feita com sucesso")
             output=""
